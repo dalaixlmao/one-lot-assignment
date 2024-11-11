f734c2266ff6d09bc0caa80e83c096254f905c7b
Anubhav <anubhav_a@me.iitr.ac.in>
2024-11-11 19:05:10 +0530

    fixed the bug of not changing colors as expected for Dollar and Speaker icons in Offer.tsx

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -1,4 +1,4 @@
-import { DescriptiveText, LargeText } from "./ui/texts"; // Importing UI components for text rendering
+import { DescriptiveText, LargeText } from "@/components/ui/texts";
 import React from "react";
 import { OfferType } from "@/lib/types";
 
@@ -10,14 +10,18 @@ export default function Offer({
   icon,
   color,
 }: OfferType) {
+  // Define the background color based on the 'color' prop
+  const backgroundColor = color === 'blue' ? 'bg-[#D6F2F5]' : 'bg-[#F5EBF9]';
+
+  // Define the text color based on the 'color' prop
+  const textColor = color === 'blue' ? 'text-[#51CAD7]' : 'text-[#57009C]';
+
   return (
     <div className="md:mt-0 mt-5 flex flex-row items-start md:pr-48">
       {/* Icon container with dynamic background color */}
       <div className="mt-1">
-        {/* Dynamically set background color based on the 'color' prop */}
-        <div 
-          className={`h-6 w-6 flex flex-col items-center rounded-full justify-center 
-                      bg-${color === 'blue' ? "[#D6F2F5]" : "[#F5EBF9]"}`}
+        <div
+          className={`h-6 w-6 flex flex-col items-center rounded-full justify-center ${backgroundColor}`}
         >
           {icon} {/* Render the icon passed as a prop */}
         </div>
@@ -31,7 +35,7 @@ export default function Offer({
           </div>
         </div>
         {/* Motive with dynamic color based on 'color' prop */}
-        <div className={`text-${color === 'blue' ? "[#51CAD7]" : "[#57009C]"} text-sm font-semibold`}>
+        <div className={`${textColor} text-sm font-semibold`}>
           {motive}
         </div>
         {/* Description of the offer */}
